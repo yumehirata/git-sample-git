544f01f33f0bd013d5b01cdc7252cc9891d40f31
hiratayume <yume.hirata@rakus.co.jp>
2018-11-19 11:30:02 +0900

    新規プロジェクト作成

---
+++ src/main/java/jp/co/rakus/GitSampleGitApplication.java
@@ -0,0 +1,12 @@
+package jp.co.rakus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleGitApplication.class, args);
+	}
+}
